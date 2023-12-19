66370c9055b6d6791b4ef9166757efd1602223ff
LostArrows27 <thelastofus2isnuts@gmail.com>
2023-12-19 22:28:46 +0700

    update UI

--- src/app/office/new-order/page.tsx
+++ src/app/office/new-order/page.tsx
@@ -3,12 +3,12 @@ import OrderProgress from "@/components/new-order-page/OrderProgress";
 
 export default function NewOrderPage() {
   return (
-    <div className="w-full h-full px-[10%] flex py-10">
-      <div className="w-1/4 h-[calc(100vh-200px)] pt-[102px]">
+    <div className="w-full h-full pl-[6%] pr-[10%] flex py-10">
+      <div className="w-1/4 h-[calc(100vh-200px)] scale-90 pt-[80px]">
         <OrderProgress />
       </div>
       <div className="w-3/4 ">
-        <div className="w-full  flex flex-col items-center">
+        <div className="w-4/5 flex flex-col items-center">
           <h1 className="font-semibold text-2xl uppercase">Create an order</h1>
           <p className="text-gray-400 my-4">
             Fill out the form below to go to next step
