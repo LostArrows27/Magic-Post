c15564619dcc625d8209732e81e45c57e5dff509
MinhMatMong <kurominhnhat@gmail.com>
2023-12-24 17:19:48 +0700

    real data and check function and search

--- src/app/office/new-transfer/page.tsx
+++ src/app/office/new-transfer/page.tsx
@@ -1,17 +1,44 @@
 import OrderList from "@/components/new-transfer/order-list";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Parcel } from "@/types/supabase-table-type";
+import { Database } from "@/types/supabase-type";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 export default async function NewTransferPage() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const { data, error } = await supabase
+    .from("parcels")
+    .select("*")
+    .eq("state", "đã nhận từ khách hàng");
+  if (error) {
+    return (
+      <div className="p-10 w-full  pt-6 min-h-full h-fit bg-border flex flex-col ">
+        <div className="mb-4 flex justify-between w-full">
+          <h1 className="font-bold text-3xl ">Create a Transfer</h1>
+        </div>
+        <div className="flex-1 bg-background flex justify-center items-center text-xl ">
+          {" "}
+          There is an error occur.
+        </div>
+      </div>
+    );
+  }
+  let parcels: (Parcel & { checked: boolean })[] = [];
+  if (data) {
+    parcels = data.map((parcel) => {
+      return {
+        ...parcel,
+        checked: false,
+      };
+    });
+  }
+
   return (
-    <div className="p-10 w-full pt-6 h-full bg-border flex flex-col ">
-     <div className="mb-4 flex justify-between w-full">
-        <h1 className="font-bold text-3xl ">
-          Create a Transfer 
-        </h1>
-     
-     </div>
-     <OrderList />
+    <div className="p-10 w-full  pt-6 min-h-full h-fit bg-border flex flex-col ">
+      <div className="mb-4 flex justify-between w-full">
+        <h1 className="font-bold text-3xl ">Create a Transfer</h1>
+      </div>
+      <OrderList parcels={parcels} />
     </div>
   );
 }
--- src/components/new-transfer/order-list.tsx
+++ src/components/new-transfer/order-list.tsx
@@ -11,37 +11,146 @@ import {
 import SearchOrder from "./search-order";
 import FilterOrder from "./filter-order";
 import { Button } from "../ui/button";
+import { Parcel } from "@/types/supabase-table-type";
+import { useCallback, useEffect, useState, useMemo, use } from "react";
+import { Checkbox } from "../ui/checkbox";
+import { useTransferOrdersList } from "@/hooks/useTransferOrdersList";
 
-export default function OrderList() {
+export default function OrderList({
+  parcels,
+}: {
+  parcels: (Parcel & {
+    checked: boolean;
+  })[];
+}) {
+  const {
+    orders,
+    displayOrders,
+    setDisplayOrders,
+    setOrders,
+    totalWeight,
+    totalFee,
+    setTotalFee,
+    setTotalWeight,
+  } = useTransferOrdersList();
+
+  const [change, setChange] = useState(false);
+
+  useEffect(() => {
+    //recalculate total weight and total fee
+    let totalWeight = 0;
+
+    let totalFee = 0;
+    orders.forEach((order) => {
+      if (order.checked) {
+        totalWeight += order.weight;
+        totalFee += order.paid_fee;
+      }
+    });
+    setTotalWeight(totalWeight);
+    setTotalFee(totalFee);
+  }, [change]);
+
+  useEffect(() => {
+    setOrders(parcels);
+    setDisplayOrders(parcels);
+  }, [parcels]);
+  //console log total fee in format 1,000,000 basiccally every 3 digits add a comma
+
   return (
-    <section className="bg-background w-full rounded-xl flex-1 flex flex-col justify-center items-center p-5">
+    <section className="bg-background w-full rounded-xl h-fit flex flex-col justify-center items-center p-5">
       <div className="flex w-full pb-5  justify-between">
         <SearchOrder />
         <FilterOrder />
       </div>
       <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableCaption className=" font-bold italic">
+          Check the orders you want to transfer
+        </TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead className="w-[60px] ">
+              <Checkbox
+                className="border-0 shadow-border"
+                checked={orders.every((ord) => ord.checked)}
+                onCheckedChange={(checked: boolean) => {
+                  //check all
+                  const newOrders = [...orders];
+                  newOrders.forEach((o) => {
+                    o.checked = checked;
+                  });
+                  setOrders(newOrders);
+                  setDisplayOrders(newOrders);
+                  setChange(!change);
+                }}
+              />
+            </TableHead>
+            <TableHead className="w-[100px]">Id</TableHead>
+            <TableHead className="w-[250px]">Name</TableHead>
+            <TableHead className="w-[250px]">Description</TableHead>
+            <TableHead>Destination</TableHead>
+            <TableHead>Weight (kg)</TableHead>
+            <TableHead>Fee (vnd)</TableHead>
+            <TableHead className="text-right">Sent Date (GMT+7)</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
+          {displayOrders.map((order) => {
+            return (
+              <TableRow key={order.id}>
+                <TableCell className="w-[60px]  ">
+                  <Checkbox
+                    checked={order.checked}
+                    onCheckedChange={(checked: boolean) => {
+                      const newOrders = [...orders];
+                      newOrders.forEach((o) => {
+                        if (order.id === o.id) {
+                          o.checked = checked;
+                          return;
+                        }
+                      });
+                      setOrders(newOrders);
+                      setDisplayOrders(newOrders);
+                      setChange(!change);
+                    }}
+                    className="shadow-border border-0"
+                  />
+                </TableCell>
+                <TableCell className="max-w-[100px] w-[100px] truncate">
+                  {order.id}
+                </TableCell>
+                <TableCell className=" max-w-[250px] w-[250px] truncate">
+                  {order.product_name}
+                </TableCell>
+                <TableCell className=" max-w-[250px] w-[250px] truncate">
+                  {order.description}
+                </TableCell>
+                <TableCell className="truncate">
+                  {order.destination_location_id}
+                </TableCell>
+                <TableCell className="truncate">{order.weight}</TableCell>
+                <TableCell className="truncate">{order.paid_fee}</TableCell>
+                <TableCell className="text-right">
+                  {new Date(order.date_sent).toLocaleString()}
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
-      <div className="mt-auto flex justify-between w-full pt-5 items-center">
-        <h2 className="flex text-xl font-bold">Total Weight: 100 kg</h2>
-        <Button className="text-lg" size={'lg'}>
-            Confirm
+      <div className="mt-auto flex justify-between w-full pt-10 items-center">
+        <div>
+          <h2 className="font-bold">
+            Total Weight: {totalWeight.toLocaleString()} kg
+          </h2>
+
+          <h2 className="font-bold">
+            Total Fee: {totalFee.toLocaleString()} vnd
+          </h2>
+        </div>
+
+        <Button className="text-lg" size={"lg"}>
+          Confirm
         </Button>
       </div>
     </section>
