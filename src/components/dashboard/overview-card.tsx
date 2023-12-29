af74f60f7c34856e99e4c3e02673ef2a79aab65f
MinhMatMong <kurominhnhat@gmail.com>
2023-12-29 19:59:05 +0700

    add the statistics for the leader

--- src/components/dashboard/overview-card.tsx
+++ src/components/dashboard/overview-card.tsx
@@ -3,29 +3,43 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { cn } from "@/lib/utils";
 
 interface SideNavProps {
-    items: OverviewItem[];
-    className?: string;
+  items: OverviewItem[];
+  className?: string;
 }
 
 export function OverviewCard({ items, className }: SideNavProps) {
-    return (
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            {items.map((item) => (
-                <Card key={item.title} className="bg-neutral-100">
-                  <CardHeader className="flex flex-row gap-2 items-center justify-start space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      {item.title}
-                    </CardTitle>
-                    <item.icon className={cn("h-4 w-4 cursor-pointer", item.color)} />
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">{item.content}</div>
-                    {/* <p className="text-xs text-muted-foreground">
+  return (
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+      {items.map((item) => {
+        const color = `text-[${item.color}]`;
+        return (
+          <Card key={item.title} className="bg-neutral-100">
+            <CardHeader className="flex flex-row gap-2 items-center justify-start space-y-0 pb-2">
+              <CardTitle className={cn("text-sm font-medium")}>
+                {item.title}
+              </CardTitle>
+              <item.icon
+                className={cn(
+                  "h-4 w-4 cursor-pointer",
+                  item.title === "Orders"
+                    ? "text-[#E1C16E]"
+                    : item.title === "Orders delivered"
+                    ? "text-[#22c35e]"
+                    : item.title === "Orders in progress"
+                    ? "text-[#0047AB]"
+                    : "text-[#ec4547]"
+                )}
+              />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{item.content}</div>
+              {/* <p className="text-xs text-muted-foreground">
                       {item.fluctuate}
                     </p> */}
-                  </CardContent>
-                </Card>
-            ))}
-      </div>
-    );
+            </CardContent>
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
--- src/components/statistics/overview-line-chart.tsx
+++ src/components/statistics/overview-line-chart.tsx
@@ -1,19 +1,167 @@
-"use client"
-import { LineChartItem } from '@/types/line-chart-item';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+"use client";
+import { LineChartItem } from "@/types/line-chart-item";
+import { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 interface OverviewLineChartProps {
-  items: LineChartItem[];
   className?: string;
 }
+// import { LineChartItem } from "@/types/line-chart-item";
+import { data_line_overview } from "@/constants/line-chart";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useSupabase } from "@/utils/supabaseClient";
+import { toast } from "sonner";
+var months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+var week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+export function OverviewLineChart({ className }: OverviewLineChartProps) {
+  const [data, setData] = useState<LineChartItem[]>([]);
+  const [filter, setFilter] = useState<"week" | "year">("year");
+  const supabaseClient = useSupabase();
+  useEffect(() => {
+    (async () => {
+      let date = new Date();
+      let initialData: any[] = [];
+      if (filter === "year") {
+        date.setFullYear(date.getFullYear() - 1);
+        initialData = months.map((month) => {
+          return {
+            name: month,
+            delivered: 0,
+            failed: 0,
+            progress: 0,
+            total: 0,
+          };
+        });
+      } else {
+        date.setDate(date.getDate() - 7);
+        initialData = week.map((day) => {
+          return {
+            name: day,
+            delivered: 0,
+            failed: 0,
+            progress: 0,
+            total: 0,
+          };
+        });
+      }
+      date.setDate(date.getDate() - 7);
+      const { data: delivered, error: deliverdError } = await supabaseClient
+        .from("parcels")
+        .select("id, date_sent")
+        .eq("state", "đã giao")
+        .gte("date_sent", date.toISOString())
+        .order("date_sent", { ascending: false });
+      const { data: totalData } = await supabaseClient
+        .from("parcels")
+        .select("id, date_sent")
+        .gte("date_sent", date.toISOString())
+        .order("date_sent", { ascending: false });
+      const { data: failed, error: failedError } = await supabaseClient
+        .from("parcels")
+        .select("id, date_sent")
+        .eq("state", "đã trả lại điểm giao dịch đích")
+        .gte("date_sent", date.toISOString())
+        .order("date_sent", { ascending: false });
+      const { data: progressData } = await supabaseClient
+        .from("parcels")
+        .select("id, date_sent")
+        .in("state", [
+          "đã nhận từ khách hàng",
+          "đang chuyển đến điểm tập kết gửi",
+          "đã nhận từ điểm giao dịch gửi",
+          "đang chuyển đến điểm tập kết đích",
+          "đã nhận từ điểm tập kết gửi",
+          "đang chuyển đến điểm giao dịch đích",
+          "đã nhận từ điểm tập kết đích",
+          "sẵn sàng giao hàng",
+        ])
+        .gte("date_sent", date.toISOString())
+        .order("date_sent", { ascending: false });
 
-export function OverviewLineChart({ items, className }: OverviewLineChartProps) {
+      if (deliverdError || failedError) {
+        toast.error(deliverdError?.message || failedError?.message);
+        return;
+      }
+
+      delivered?.forEach((par) => {
+        const parDate = new Date(par.date_sent);
+        const parMonth = parDate.getMonth();
+        const parDay = parDate.getDay();
+        if (filter === "year") {
+          initialData[parMonth].delivered += 1;
+        } else {
+          initialData[parDay].delivered += 1;
+        }
+      });
+      failed?.forEach((par) => {
+        const parDate = new Date(par.date_sent);
+        const parMonth = parDate.getMonth();
+        const parDay = parDate.getDay();
+        if (filter === "year") {
+          initialData[parMonth].failed += 1;
+        } else {
+          initialData[parDay].failed += 1;
+        }
+      });
+      totalData?.forEach((par) => {
+        const parDate = new Date(par.date_sent);
+        const parMonth = parDate.getMonth();
+        const parDay = parDate.getDay();
+        if (filter === "year") {
+          initialData[parMonth].total += 1;
+        } else {
+          initialData[parDay].total += 1;
+        }
+      });
+      progressData?.forEach((par) => {
+        const parDate = new Date(par.date_sent);
+        const parMonth = parDate.getMonth();
+        const parDay = parDate.getDay();
+        if (filter === "year") {
+          initialData[parMonth].progress += 1;
+        } else {
+          initialData[parDay].progress += 1;
+        }
+      });
+      console.log(initialData);
+      setData(initialData);
+    })();
+  }, [filter]);
   return (
-    <ResponsiveContainer width="100%" height={450}>
+    <div className="w-full relative">
+      <ResponsiveContainer width="100%" height={450}>
         <LineChart
           width={500}
           height={300}
-          data={items}
+          data={data}
           margin={{
             top: 5,
             right: 30,
@@ -26,9 +174,48 @@ export function OverviewLineChart({ items, className }: OverviewLineChartProps) 
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="delivered" stroke="#22c35e" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="failed" stroke="#ec4547" />
+
+          <Line
+            type="monotone"
+            dataKey="total"
+            stroke="#E1C16E"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="delivered"
+            stroke="#22c35e"
+            activeDot={{ r: 8 }}
+          />
+
+          <Line
+            type="monotone"
+            dataKey="progress"
+            stroke="#0047AB"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="failed"
+            stroke="#ec4547"
+            activeDot={{ r: 8 }}
+          />
         </LineChart>
-    </ResponsiveContainer>
-  )
+      </ResponsiveContainer>
+      <Select
+        value={filter}
+        onValueChange={(value) => {
+          setFilter(value as "week" | "year");
+        }}
+      >
+        <SelectTrigger className="w-[150px] bg-primary text-primary-foreground absolute right-0 -top-14  ">
+          <SelectValue placeholder="Choose filter" />
+        </SelectTrigger>
+        <SelectContent className="bg-primary text-primary-foreground">
+          <SelectItem value={"week"}>Week</SelectItem>
+          <SelectItem value={"year"}>Year</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
 }
